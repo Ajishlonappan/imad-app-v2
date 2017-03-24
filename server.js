d114eaff3d8b58de071cae680442fcbac56f297d
Ajishlonappan <ajishlonappan@gmail.com>
2017-03-24 21:47:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
- res.send(createTemplate(articles[articleName]));
+ res.send(createTemplate(articles[articlesName]));
 });
 
 app.get('/ui/madi.png', function (req, res) {

931da903917a090801783aa8c9537e1df9337037
Ajishlonappan <ajishlonappan@gmail.com>
2017-03-24 21:51:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
+    var articlesName = req.params.articleName;
  res.send(createTemplate(articles[articlesName]));
 });
 

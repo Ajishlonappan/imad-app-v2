47d3793e56185fbde15e04ba0ebca2226a4f9f76
Ajishlonappan <ajishlonappan@gmail.com>
2017-03-25 20:58:45 +0530

    API linking

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,20 @@
-console.log('Loaded!');
-var element= document.getElementById('main text')
-element.innerHTML="New vlue"
-var img= document.getElementById('madi')
-
-var marginLeft=0
-function moveRight(){
-  marginLeft= marginLeft + 1;
-  img.style.marginLeft= marginLeft+'px';
+//counter  code
+var button= document.getElementById('counter')
+button.onclick = function(){
+//create a requets
+var request= new XMLHttpRequest();
+//Capture and create a response
+request.onreadystatechange = function(){
+  if(request.readyState===XMLHttpRequest.DONE){
+    if(request.status===200){
+      var counter= request.responseText;
+      var span = document.getElementById('count');
+      span.innerHTML = counter.toString();
+    }
+  }
 }
-img.onclick=  function(){
-  var interval = setInterval(moveRight,50);
+// Make request
+request.open('GET','http://127.0.0.1:8080/counter', true);
+request.send(null);
+
 };

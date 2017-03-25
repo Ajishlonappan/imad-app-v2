e6179f6b56a5f22f5811a412cb059812ad0e8596
Ajishlonappan <ajishlonappan@gmail.com>
2017-03-25 23:16:42 +0530

    api checkwith url /:name params

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,19 @@ request.open('GET','http://127.0.0.1:8080/counter', true);
 request.send(null);
 
 };
+
+//sumit name
+var nameInput =document.getElementById('name');
+var name= nameInput.value;
+var submit=document.getElementById('submit_btn')
+submit.onclick = function(){
+  // Send the list
+
+var list='';
+for (var i = 0; i < names.length; i++) {
+  list +='<li>' + names[i]+'</li>';
+}
+
+var ul=document.getElementById('namelist');
+ul.innerHTML=list;
+}
